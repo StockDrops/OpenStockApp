d37e2212bb7352c65eb7358344ffe37f6e0eae31
Henry <emorell96@hotmail.com>
2022-04-13 23:35:25 +0200

    continued styling the mobile app.

--- OpenStockApp/App.xaml.cs
+++ OpenStockApp/App.xaml.cs
@@ -2,7 +2,10 @@
 using OpenStockApp.Models;
 using OpenStockApp.Pages;
 using OpenStockApp.Services;
-
+using Microsoft.Maui.Platform;
+#if ANDROID
+using Android.Content.Res;
+#endif
 namespace OpenStockApp;
 
 public partial class App : Application
@@ -10,6 +13,15 @@ public partial class App : Application
 	public App()
 	{
         ThemeSelectorService.InitializeTheme();
+
+#if ANDROID
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("LightUnderline", (h, v) =>
+        {
+            h.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Colors.LightGray.ToPlatform());
+        });
+#endif
+
+
         InitializeComponent();
         
         if (AppConfig.Desktop)
--- OpenStockApp/Pages/Alerts/NotificationsPageMobile.xaml.cs
+++ OpenStockApp/Pages/Alerts/NotificationsPageMobile.xaml.cs
@@ -17,21 +17,21 @@ namespace OpenStockApp.Pages.Alerts
             //var converter = new CommunityToolkit.Maui.Converters.IsStringNotNullOrEmptyConverter();
             Behaviors.Add(new EventToCommandBehavior { EventName = nameof(this.Appearing), Command = notificationsPageViewModel.NavigateToPage }); //TODO: One day I'd like to know why this is needed, and why the XAML binding is not working.
             //Behaviors.Add(new EventToCommandBehavior { EventName = nameof(this.Appearing), Command = notificationsPageViewModel.NavigateToPage });
-            //notificationsPageViewModel.ScrollTo = OnScrollTo;
-            notificationsPageViewModel.SetBinding(NotificationsPageViewModel.ListHeightProperty, new Binding(nameof(Height), source: listView));
+            notificationsPageViewModel.ScrollTo = OnScrollTo;
+            notificationsPageViewModel.SetBinding(NotificationsPageViewModel.ListHeightProperty, new Binding(nameof(Height), source: collectionView));
         }
 
         
         public void OnScrollTo(Result result)
         {
-            
+#if ANDROID || IOS
             try
             {
-                //if (result != null)
-                //    listView?.ScrollTo(result, ScrollToPosition.End, true);
+                if (result != null)
+                    collectionView?.ScrollTo(0, position: ScrollToPosition.Start);
             }
             catch { }
-             
+#endif  
         }
     }
 }
