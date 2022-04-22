7009026bac057445963123ea97d69d14a8c9d548
Henry <emorell96@hotmail.com>
2022-04-23 00:21:08 +0200

    modified the enum to int converter to return null if value = -1 but if not do as before.

--- OpenStockApp.Core.Maui/Converters/EnumToIntConverter.cs
+++ OpenStockApp.Core.Maui/Converters/EnumToIntConverter.cs
@@ -9,10 +9,11 @@ using System.Threading.Tasks;
 
 namespace OpenStockApp.Core.Maui.Converters
 {
+#nullable enable
 	/// <summary>
 	///     Converts an <see cref="Enum" /> to its underlying <see cref="int" /> value.
 	/// </summary>
-	public class EnumToIntConverter : BaseConverter<Enum, int, Type>
+	public class EnumToIntConverter : BaseConverter<Enum?, int, Type>
 	{
 		/// <summary>
 		/// Convert a default <see cref="Enum"/> (i.e., extending <see cref="int"/>) to corresponding underlying <see cref="int"/>
@@ -22,7 +23,7 @@ namespace OpenStockApp.Core.Maui.Converters
 		/// <param name="culture">Unused: Culture to use in the converter</param>
 		/// <returns>The underlying <see cref="int"/> value of the passed enum value</returns>
 		/// <exception cref="ArgumentException">If value is not an enumeration type</exception>
-		public override int ConvertFrom(Enum value, Type? parameter = null, CultureInfo? culture = null)
+		public override int ConvertFrom(Enum? value, Type? parameter = null, CultureInfo? culture = null)
 		{
 			ArgumentNullException.ThrowIfNull(value);
 
@@ -36,15 +37,19 @@ namespace OpenStockApp.Core.Maui.Converters
 		/// <param name="parameter"></param>
 		/// <param name="culture">Unused: Culture to use in the converter</param>
 		/// <returns>The underlying <see cref="Enum"/> of the associated targetType</returns>
-		/// <exception cref="ArgumentException">If value is not a valid value in the targetType enum</exception>
+		/// <exception cref="ArgumentException">If value is not a valid value in the targetType enum except for -1 which is the default value for SelectedIndex in Picker</exception>
 		public override Enum? ConvertBackTo(int value, Type parameter, CultureInfo? culture = null)
 		{
+			if (value == -1)
+            {
+				return null;
+			}
+			
 			ArgumentNullException.ThrowIfNull(value);
 			ArgumentNullException.ThrowIfNull(parameter);
 
 			if (!Enum.IsDefined(parameter, value))
 			{
-				return null;
 				throw new InvalidEnumArgumentException($"{value} is not valid for {parameter.Name}");
 			}
 
